3e09c82b6e4d8b18a38085bead8b3d3525b36e41
Rudra Desai <52051508+rudra-desai@users.noreply.github.com>
2020-12-14 14:41:11 -0500

    Fixed linting problems

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const ig = require("./instagram");
+const ig = require('./instagram');
 const prompt = require('prompt-sync')();
-console.log("");
+
+console.log('');
 const username = prompt('Enter your Instagram username: ');
 const password = prompt('Enter your Instagram password: ');
 
-(async() => {
-    await ig.initialize();
-    await ig.login(username, password);
-    await ig.fetchFollowers();
-    await ig.fetchFollowing_and_compare();
-})()
+(async () => {
+  await ig.initialize();
+  await ig.login(username, password);
+  await ig.fetchFollowers();
+  await ig.fetchFollowing_and_compare();
+})();
